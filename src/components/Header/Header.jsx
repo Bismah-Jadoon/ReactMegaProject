4fad0ab2cb8e31ae861bf1e6bee95daf58d585a4
Bismah-Jadoon <khanbismah63@gmail.com>
2024-02-25 13:52:01 +0500

    project

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -39,7 +39,15 @@ export default function Header() {
   return (
     <header className="py-3 shadow bg-gray-500">
       <Container>
-        <nav></nav>
+        <nav className="flex">
+          <div className="mr-4">
+          <Link to='/'>
+            <Logo width="70px" />
+          </Link>
+          </div>
+
+          <ul className="flex ml-auto"></ul>
+        </nav>
       </Container>
     </header>
   );

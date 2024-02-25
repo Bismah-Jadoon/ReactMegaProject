53d3d8190b439114172d17a53fdd5267aeda1ba2
Bismah-Jadoon <khanbismah63@gmail.com>
2024-02-25 09:37:34 +0500

    project

---
+++ src/components/Header/LogoutButton.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import authService from "../../appwrite/config";
+import { logout } from "../../store/authSlice";
+
+export default function LogoutButton() {
+  const dispatch = useDispatch();
+  const logoutHandler = () => {
+    authService.logout().then(() => {
+      dispatch(logout());
+    }).catch(error);
+  };
+
+  return <div></div>;
+}

4b9231ef1eb054335819ceeb4b6c0e906f65ec0f
Bismah-Jadoon <khanbismah63@gmail.com>
2024-02-25 09:55:53 +0500

    project

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import { Container, Logo, LogoutButton } from "../index";
+import { Link } from "react-router-dom";
+import { UseSelector, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
-  return (
-    <div>
-      Header
-    </div>
-  )
+  const authStatus = useSelector((state) => state.auth.status);
+  return <div>Header</div>;
 }

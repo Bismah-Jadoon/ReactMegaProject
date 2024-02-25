d521dec06d64e94c6d2b01ebaa891aa4a4310cb4
Bismah-Jadoon <khanbismah63@gmail.com>
2024-02-25 14:41:34 +0500

    project

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,6 +1,12 @@
 import React, { useId } from "react";
 
-const Input = React.forwardRef( function Input(){
+const Input = React.forwardRef( function Input({
+    label,
+    type = "text",
+    className = "",
+    ...props
+
+}, ref){
     return <h1>Test</h1>
 })
 

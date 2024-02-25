d46458f549891bfb701f213f2c0d86ef9c849e26
Bismah-Jadoon <khanbismah63@gmail.com>
2024-02-25 09:40:50 +0500

    project

--- src/components/Header/LogoutButton.jsx
+++ src/components/Header/LogoutButton.jsx
@@ -11,5 +11,5 @@ export default function LogoutButton() {
     }).catch(error);
   };
 
-  return <div></div>;
+  return <button className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full ">Logout</button>;
 }

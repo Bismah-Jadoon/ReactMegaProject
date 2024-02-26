0f716b319dcffe741fab8f20f3b5b28db4f61b35
Bismah-Jadoon <khanbismah63@gmail.com>
2024-02-26 10:58:40 +0500

    project

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -2,8 +2,16 @@ import React, {useState} from 'react'
 import authService from '../appwrite/auth'
 import {Link, useNavigate} from 'react-router-dom'
 import { login } from '../store/authSlice'
+import {Button , Input, Logo} from './index'
+import { UseDispatch, useDispatch } from 'react-redux'
+import { useForm } from 'react-hook-form' 
 
 function Signup() {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const [error, setError] = useState("")
+    const {register, handleSubmit} = useForm()
+    
   return (
     <div>
       

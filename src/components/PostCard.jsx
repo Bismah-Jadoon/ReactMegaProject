3ccc96296230c11f2d8ba13ae11cace3772ccba2
Bismah-Jadoon <khanbismah63@gmail.com>
2024-02-26 09:59:27 +0500

    project

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -7,8 +7,9 @@ function PostCard({$id, title,featuredImage}) {
     <Link to={`/post/${$id}`}>
       <div className='w-full bg-gray-100 rounded-xl p-4'>
        <div className='w-full justify-center mb-4'>
-        
+        <img src={} alt='' />
        </div>
+       <h2>{title}</h2>
       </div>
     </Link>
   )

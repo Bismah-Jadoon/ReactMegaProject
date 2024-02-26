ae6f58e14abde6d74312f083c32025a0e1f026d4
Bismah-Jadoon <khanbismah63@gmail.com>
2024-02-26 10:15:54 +0500

    project

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,6 +7,15 @@ import authService from '../appwrite/auth'
 import {useForm} from 'react-hook-form'
 
 function Login() {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const {register, handleSubmit} = useForm()
+    const [error, setError]  = useState("")
+
+    const login = async(data) => {
+        setError("")
+    }
+
   return (
     <div>
       

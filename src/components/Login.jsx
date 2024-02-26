d4f0bba87f35371050979e80188f64e344a9af4e
Bismah-Jadoon <khanbismah63@gmail.com>
2024-02-26 10:23:23 +0500

    project

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,31 +1,32 @@
-import React, {useState} from 'react'
-import {Link, useNavigate} from 'react-router-dom'
-import {login as authLogin } from '../store/authSlice'
-import {Button, Input, Logo} from './index'
-import { UseDispatch } from 'react-redux'
-import authService from '../appwrite/auth'
-import {useForm} from 'react-hook-form'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { login as authLogin } from "../store/authSlice";
+import { Button, Input, Logo } from "./index";
+import { UseDispatch } from "react-redux";
+import authService from "../appwrite/auth";
+import { useForm } from "react-hook-form";
 
 function Login() {
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
-    const {register, handleSubmit} = useForm()
-    const [error, setError]  = useState("")
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { register, handleSubmit } = useForm();
+  const [error, setError] = useState("");
 
-    const login = async(data) => {
-        setError("")
-        try {
-           const session  = await authService.login(data) 
-        } catch (error) {
-            setError(error.message)
-        }
+  const login = async (data) => {
+    setError("");
+    try {
+      const session = await authService.login(data);
+      if (session) {
+        userData = await authService.getCurrentUser()
+        if(userData)dispatch(authLogin(userData))
+        navigate("/")
+      }
+    } catch (error) {
+      setError(error.message);
     }
+  };
 
-  return (
-    <div>
-      
-    </div>
-  )
+  return <div></div>;
 }
 
-export default Login
+export default Login;

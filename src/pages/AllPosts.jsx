09781ba0fb046b6fa2780ae3f7e8e1caa9cee330
Bismah-Jadoon <khanbismah63@gmail.com>
2024-02-27 11:10:23 +0500

    project

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -7,6 +7,17 @@ export default function AllPosts() {
 
     const [posts, setPosts] = useState([])
     useEffect(() => {}, [])
-    appwriteService.getPosts([]).then((posts) => setPosts(posts))
-  return
+    appwriteService.getPosts([]).then((posts) =>{
+      if(posts){
+        setPosts(posts.documents)
+      }
+    } 
+   )
+  return(
+    <div className="w-full py-8">
+      <Container>
+        
+      </Container>
+    </div>
+  )
 }

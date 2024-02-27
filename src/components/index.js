cb7d52a24140931595163f30aba0108de6db8bb8
Bismah-Jadoon <khanbismah63@gmail.com>
2024-02-27 10:49:59 +0500

    project

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,24 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
-import LogoutButton from "./Header/LogoutButton"
-import RTE from './RTE'
+import LogoutButton from "./Header/LogoutButton";
+import RTE from "./RTE";
+import Signup from "../pages/Signup";
+import Login from "../pages/Login";
+import AddPost from "../pages/AddPost";
+import PostForm from './post-form/PostForm'
+import PostCard from './PostCard'
 
-export { Header, Footer, Container, Logo, LogoutButton, RTE};
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutButton,
+  RTE,
+  Signup,
+  Login,
+  AddPost,
+  PostForm,
+  PostCard
+};

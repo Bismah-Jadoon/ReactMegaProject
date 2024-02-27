a12f00a254b0845b902d43f7ccd27b344f11d8ec
Bismah-Jadoon <khanbismah63@gmail.com>
2024-02-27 11:33:06 +0500

    project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,33 @@ import appwriteService from "../appwrite/config";
 import { Container, PostCard } from "../components";
 
 export default function Home() {
-    cnst [posts, setPosts] = useState([])
+  cnst[(posts, setPosts)] = useState([]);
 
-    useEffect(() => {
-        appwriteService.getPosts().then((posts) => {
-            if(posts) {
-                setPosts(posts.documents)
-            }
-        })
-    }, [])
-  return <div></div>;
+  useEffect(() => {
+    appwriteService.getPosts().then((posts) => {
+      if (posts) {
+        setPosts(posts.documents);
+      }
+    });
+  }, []);
+
+  if (posts.length === 0) {
+    return (
+      <div className="w-full py-8 mt-4 text-center">
+        <Container>
+          <div className="flex flex-wrap">
+            <div className="p-2 w-full">
+              <h1 className="text-2xl font-bold hover:text-gray-500">
+                Login to read posts
+              </h1>
+            </div>
+          </div>
+        </Container>
+      </div>
+    );
+  }
+
+  return <div className="w-full py-8">
+    
+  </div>;
 }

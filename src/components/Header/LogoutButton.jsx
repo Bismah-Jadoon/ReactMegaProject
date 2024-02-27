2bd9f95d129a9f1a03b45a9997720974644b1fcf
Bismah-Jadoon <khanbismah63@gmail.com>
2024-02-27 16:53:16 +0500

    project

--- src/components/Header/LogoutButton.jsx
+++ src/components/Header/LogoutButton.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import authService from "../../appwrite/config";
+import authService from "../../appwrite/auth";
 import { logout } from "../../store/authSlice";
 
 export default function LogoutButton() {
   const dispatch = useDispatch();
   const logoutHandler = () => {
-    authService.logout().then(() => {
-      dispatch(logout());
-    }).catch(error);
+    authService
+      .logout()
+      .then(() => {
+        dispatch(logout());
+      })
+      .catch(error);
   };
 
-  return <button className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full ">Logout</button>;
+  return (
+    <button
+      className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full "
+      onClick={logoutHandler}
+    >
+      Logout
+    </button>
+  );
 }

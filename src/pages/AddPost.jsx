deb00c3bc7cc6925806b76eaf4207fc652d27262
Bismah-Jadoon <khanbismah63@gmail.com>
2024-02-28 10:02:35 +0500

    Mega Blog Complex Project

--- src/pages/AddPost.jsx
+++ src/pages/AddPost.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import { Container, PostForm } from "../components";
+import React from 'react'
+import { Container, PostForm } from '../components'
 
-export default function AddPost() {
+function AddPost() {
   return (
-    <div className="py-8">
-      <Container>
-        <PostForm />
-      </Container>
+    <div className='py-8'>
+        <Container>
+            <PostForm />
+        </Container>
     </div>
-  );
+  )
 }
+
+export default AddPost
